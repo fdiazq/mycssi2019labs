fbf294b076786bb83d68d0721b5983eb8f6e59c0
Francisco Diaz <diaz274@csusm.edu>
2019-07-24 12:00:38 -0700

    css/javascript

---
+++ WeLearn/M2-javaScript/L1-JSIntro/script.js
@@ -0,0 +1,12 @@
+console.log("script is running");
+let name = 'Charlie';
+console.log(' Hello ' + name + '!');
+let age = 15;
+console.log( ' You are ' + age + ' years old! ');
+
+if (Age >= 16){
+    console.log("I can drive")
+}
+else{
+    console.log("I cannot drive")
+  }
